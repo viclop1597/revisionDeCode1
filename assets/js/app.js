c08b7febe2223069b48ea78d82b864904d70f10e
Victor Lopez <victor.rodi25@gmail.com>
2022-10-28 14:38:36 -0500

    final final: ya salió

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,24 @@
 /* El código, por lo que entiendo, entra a una API, en la cual recuperamos el nombre, el numero de blog y la localización.
-Pero debido a que al parecer nada de eso existe en el documento, nos manda un error. */
+Pero si no encuentra la info, nos manda un error. */
 
 /* Reasigné la variable usersEndPoint para eliminar una linea de code y le cambié el nombre siguiendo CamelCase*/
 const usersEndPoint = 'https://api.github.com/users';
-/* Cambié el nombre de las variables, ya que empezaban con un $  */
-/* Linkee bien */
-const n = document.querySelector('.name');
-const b = document.querySelector('.blog');
-const l = document.querySelector('.location');
+/* Linkee bien el querySelector*/
+const $n = document.querySelector('p.name');
+const $b = document.querySelector('p.blog');
+const $l = document.querySelector('p.location');
 
 /* Agregué el async para que el await funcionara */
 async function displayUser(username) {
-  n.textContent = 'cargando...';
+  $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndPoint}/${username}`);
+  /* Definimos el data */
+  const data = await response.json();
   console.log(data);
-  n.textContent = `${data.name}`;
-  b.textContent = `${data.blog}`;
-  l.textContent = `${data.location}`;
+  /* Diferentes comillas para definirlas */
+  $n.textContent = `${data.name}`; 
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
